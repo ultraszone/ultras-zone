60c655eeafb314dc2e724b20c8a15c57dfc5f4f8
ultraszone <maltinisik1991@gmail.com>
2026-04-18 06:37:42 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import UltrasZone from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UltrasZone />
+  </React.StrictMode>
+)
